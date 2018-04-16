6061fb8e5746333fa5c683348aed5c836a11fa10
Guillaume Desjardins-Marceau <1540512@cgmatane.qc.ca>
2018-04-16 16:51:40 -0400

    ajout de la classe joueur, debut de gestion de vue, ajouter init et commencer ds client.js

---
+++ src/client/Joueur.js
@@ -0,0 +1,14 @@
+function Joueur(nom, couleur){
+  function initialiser()
+  {
+
+  }
+  function dessiner(couleur)
+  {
+
+  }
+  initialiser();
+}
+Joueur.configuration = {
+  vitesse:5
+}
--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,20 @@
-var socket = new WebSocket('ws://localhost:8888','echo-protocol');
+var socket;
+var htmlAcceuil;
+var htmlJeu;
+
 function initialiser()
 {
-  socket.send("test");
+  socket = new WebSocket('ws://localhost:8888','echo-protocol');
+  htmlAcceuil = document.getElementById("accueil").innerHTML;
+  htmlJeu = document.getElementById("jeu").innerHTML;
+
+  document.body.innerHTML = htmlAcceuil;
+}
+function commencer()
+{
+ return false;
+}
+initialiser();
+window.configuration = {
+
 }
