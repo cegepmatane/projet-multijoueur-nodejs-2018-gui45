7a86386c521ca4a8763dda876ef77af74c8f89ce
gui45 <guillaume.d.marceau@gmail.com>
2018-04-22 15:16:21 -0400

    ajouter une classe balles

---
+++ src/client/balle.js
@@ -0,0 +1,43 @@
+function Balle (nomPropriertaire, destination, depart){
+	var forme;
+	var animationJoueur;
+	var intervalBouger;
+	function initialiser()
+	{
+		animationJoueur =  joueur.getAnimation();
+		forme = new createjs.Shape();
+		forme.graphics.beginFill("black").drawCircle(0, 0, 10);
+		forme.x = depart['x'];
+		forme.y = depart['y'];
+		scene.addChild(forme);
+		intervalBouger = setInterval(bouger,10);
+		//console.log(forme.x);
+	}
+	function bouger()
+	{
+		var distanceX = (destination['x']) - forme.x;
+		var distanceY = (destination['y']) - forme.y;
+		var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+		//console.log(distanceX);
+		forme.x = forme.x + Balle.configuration.vitesse * distanceX/distance;
+		forme.y = forme.y + Balle.configuration.vitesse * distanceY/distance;
+    if(destination['x'] == forme.x && destination['y'] == forme.y)
+      scene.removeChild(forme);
+	}
+  this.getNomProprietaire = function()
+  {
+    return nomPropriertaire;
+  }
+  this.getPositionX = function()
+  {
+    return forme.x;
+  }
+  this.getPositionY = function()
+  {
+    return forme.y;
+  }
+	initialiser();
+}
+Balle.configuration= {
+	vitesse:3
+}
