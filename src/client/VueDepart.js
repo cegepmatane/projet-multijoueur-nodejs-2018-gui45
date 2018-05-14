01861b01d3e3ad83312a745f20f841c76be79305
gui45 <guillaume.d.marceau@gmail.com>
2018-05-13 22:09:31 -0400

    ajouter svgg pour le joueur

--- src/client/VueDepart.js
+++ src/client/VueDepart.js
@@ -1,6 +1,9 @@
 function VueDepart()
 {
 	var corp;
+	var canvas;
+	var vaisseau;
+	var scene;
 	function initialiser()
 	{
 		corp = document.body;
@@ -8,8 +11,24 @@ function VueDepart()
 	this.afficher = function()
 	{
 		corp.innerHTML = VueDepart.pageDepartHTML;
-
+		canvas = document.getElementById("apercu")
+		scene = new createjs.Stage(canvas);
+		chargeur = new createjs.LoadQueue(false);
+		chargeur.on('complete', terminer);
+		chargeur.loadManifest({
+			id:'svg',
+			src:'images/vaisseau.svg',
+			type: createjs.AbstractLoader.IMAGE
+		})
 	}
-	initialiser();
+	function terminer(evenement)
+	{
+		forme = new createjs.Bitmap(chargeur.getResult('svg'));
+		scene.addChild(forme);
+		//console.log("ajouter");
+	}
+
+		initialiser();
 }
 VueDepart.pageDepartHTML = document.getElementById("accueil").innerHTML;
+VueDepart.vaisseau = "images/vaisseau.svg";
