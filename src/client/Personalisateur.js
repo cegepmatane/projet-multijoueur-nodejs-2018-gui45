06fbbbd3389c404e745c9475958dc41ee19ed934
gui45 <guillaume.d.marceau@gmail.com>
2018-05-20 17:23:12 -0400

    bouton charger/sauvegarde fonctionnel

--- src/client/Personalisateur.js
+++ src/client/Personalisateur.js
@@ -1,21 +1,26 @@
 function Personalisateur()
 {
+	var bouttonSauvegarder=document.getElementById("sauvgarder");
+	var bouttonCharger=document.getElementById("charger");
  	var svg=document.getElementById("apercu");
-	var flame1=document.getElementById("flame1");
-	var flame2=document.getElementById("flame2");
-	var flame3=document.getElementById("flame3");
-	var corp1=document.getElementById("corp1");
-	var corp2=document.getElementById("corp2");
-	var corp3=document.getElementById("corp3");
-	var corp4=document.getElementById("corp4");
+	var flame1;
+	var flame2;
+	var flame3;
+	var corp1;
+	var corp2;
+	var corp3;
+	var corp4;
 	var formulaire=document.getElementById("formModification");
 	var courleurVaisseau=document.getElementById("vaisseau");
 	var courleurFlames=document.getElementById("flames");
 	function initialiser()
 	{
   	//console.log(svg.innerHTML);
+		chargerElementsSvg();
 		courleurVaisseau.addEventListener('input', changerCouleurVaisseau);
 		courleurFlames.addEventListener('input', changerCouleurFlames);
+		bouttonSauvegarder.addEventListener('click', sauvgarder);
+		bouttonCharger.addEventListener('click', charger);
 	}
 	function changerCouleurVaisseau(evenement){
 		corp1.style.fill = courleurVaisseau.value;
@@ -27,6 +32,27 @@ function Personalisateur()
 		flame1.style.fill = courleurFlames.value;
 		flame2.style.fill = courleurFlames.value;
 		flame3.style.fill = courleurFlames.value;
+	}
+	function sauvgarder(){
+		contenu = JSON.stringify(svg.innerHTML);
+		//console.log(contenu);
+		localStorage.setItem("image", contenu);
+	}
+	function charger(){
+		contenu = localStorage.getItem("image");
+		//console.log(contenu);
+		if(contenu.length != 0)
+			svg.innerHTML = JSON.parse(contenu);
+		chargerElementsSvg();
+	}
+	function chargerElementsSvg(){
+		flame1=document.getElementById("flame1");
+		flame2=document.getElementById("flame2");
+		flame3=document.getElementById("flame3");
+		corp1=document.getElementById("corp1");
+		corp2=document.getElementById("corp2");
+		corp3=document.getElementById("corp3");
+		corp4=document.getElementById("corp4");
 	}
 	initialiser();
 }
